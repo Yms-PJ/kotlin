ca9a630d53c4fb07037b578f3171a8d8fe206038
Yms-PJ <alstn0695@naver.com>
2021-10-03 08:42:13 +0900

    20211003

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,16 +1,13 @@
 package com.example.mvc.controller.get
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController         // REST API Controller
 @RequestMapping(value = ["/api"]) // http://localhost:8080/api
 class GetApiController {
 
     @GetMapping(path = ["/hello", "/abcd"]) // GET http://localhost:8080/api/hello, GET http://localhost:8080/api/abcd
-    fun hello(): String{
+    fun hello(): String {
         return "hello kotlin"
     }
 
@@ -19,5 +16,18 @@ class GetApiController {
         return "request-mappipng"
     }
 
+    @GetMapping(value = ["/get-mapping/path-variable/{name}/{age}"]) // GET http://localhost:8080/api/get-mapping/path-variable/steve/30
+    fun pathVariable(@PathVariable name: String, @PathVariable age: Int): String {
+        println("${name}, ${age}")
+        return name + " " + age
+    }
+
+    @GetMapping(value = ["/get-mapping/path-variable2/{name}/{age}"]) // GET http://localhost:8080/api/get-mapping/path-variable2/steve/30
+    fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") _age: Int): String {
+        val name: String = "yang"
+        var age: Int = 99
+        println("${_name}, ${_age}")
+        return _name + " " + _age
+    }
 
 }
