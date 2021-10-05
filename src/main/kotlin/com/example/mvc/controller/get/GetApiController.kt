5c151de9bcb1bf343e397e0bf3a215dfd919980e
Yms-PJ <alstn0695@naver.com>
2021-10-06 08:36:31 +0900

    20211006

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -34,8 +34,8 @@ class GetApiController {
     // http://localhost:8080/api/get-mapping/query-param?name=steve&age=30
     @GetMapping(value = ["/get-mapping/query-param"]) // name=steve & age = 30
     fun queryParam(
-        @RequestParam(name= "name") name: String,
-        @RequestParam(value= "age") age: Int
+        @RequestParam(name = "name") name: String,
+        @RequestParam(value = "age") age: Int
     ): String {
         println("${name}, ${age}")
         return name + " " + age
@@ -43,9 +43,16 @@ class GetApiController {
 
     // name, age, address, email
     @GetMapping(value = ["/get-mapping/query-param/object"])
-    fun queryParamObject(userRequest: UserRequest): UserRequest{
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
         println(userRequest)
         return userRequest
+    }
+
+    // map
+    @GetMapping(value = ["/get-mapping/query-param/map"])
+    fun queryParamMap(map: Map<String, Any>) : Map<String, Any>? {
+        println(map)
+        return map
     }
 
 }
